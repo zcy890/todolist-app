2165f39f1f97897696e13b5f6de57c0c61f9d385
Chris Yan <zcy890@gmail.com>
2025-05-19 20:31:12 -0700

    feat(pg): add connection

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
+const { Pool } = require("pg");
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todolist",
-  password: "Admin123$",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
